d7e323e13fe6ecda42d6663ff100aae0c9fbe7a4
Tomas Pichal <tomas@pichal.cz>
2012-09-19 08:12:42 +0200

    asdda
    asads

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mojeaplikace::Application.config.secret_token = 'f3cf25c5ddbef89e76b412cdf8e62f147f1233896ee368a59f7dcdbc768e3c6329cc0c3563ae352b9b44dd04cc955f805d79cff233bbb9ab1bcc442ae15e89c6'
